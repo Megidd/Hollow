83734cb4ceafeebde69146982c7ed17c379b7ae9
Megidd <megiddgit@gmail.com>
2024-01-12 06:17:39 -0800

    Finish up C# code

--- RhinoCommon/Hollow/Hollow/HollowMesh.cs
+++ RhinoCommon/Hollow/Hollow/HollowMesh.cs
@@ -1,10 +1,13 @@
 ﻿using Rhino;
 using Rhino.Commands;
+using Rhino.DocObjects;
 using Rhino.Geometry;
 using Rhino.Input;
 using Rhino.Input.Custom;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
 
 namespace Hollow
 {
@@ -23,9 +26,114 @@ namespace Hollow
         ///<returns>The command name as it appears on the Rhino command line.</returns>
         public override string EnglishName => "HollowMesh";
 
+        private static RhinoDoc docCurrent; // Accessed by async post-process code.
+        private static RhinoObject inObj = null; // Input object.
+        
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
+            docCurrent = doc; // Accessed by async post-process code.
+            inObj = Helper.GetInputStl(Paths.stlIn);
+            if (inObj == null)
+            {
+                return Result.Failure;
+            }
+
+            bool infill = Helper.GetYesNoFromUser("Do you want infill for hollowed mesh?");
+
+            float thickness = Helper.GetFloatFromUser(1.8, 0.0, 100.0, "Enter wall thickness for hollowing.");
+
+            uint precision = Helper.GetUint32FromUser("Enter precision: Low=1, Medium=2, High=3", 2, 1, 3);
+            switch (precision)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    break;
+                default:
+                    RhinoApp.WriteLine("Precision must be 1, 2, or 3 i.e. Low=1, Medium=2, High=3");
+                    return Result.Failure;
+            }
+
+            // Prepare arguments as text fields.
+            string args = "";
+            args += Paths.stlIn;
+            args += " ";
+            args += infill ? "true" : "false";
+            args += " ";
+            args += thickness.ToString();
+            args += " ";
+            args += precision.ToString();
+            args += " ";
+            args
+            += Paths.stlOut;
+
+            Helper.RunLogic(Paths.logic, args, PostProcess);
+
+            RhinoApp.WriteLine("Process is started. Please wait...");
+
             return Result.Success;
+        }
+
+        private static void PostProcess(object sender, EventArgs e)
+        {
+            try
+            {
+                RhinoApp.WriteLine("Post process started.");
+                Mesh meshOut = Helper.LoadStlAsMesh(Paths.stlOut);
+
+                // Run the CheckValidity method on the mesh.
+                MeshCheckParameters parameters = new MeshCheckParameters();
+                // Enable all the checks:
+                parameters.CheckForBadNormals = true;
+                parameters.CheckForDegenerateFaces = true;
+                parameters.CheckForDisjointMeshes = true;
+                parameters.CheckForDuplicateFaces = true;
+                parameters.CheckForExtremelyShortEdges = true;
+                parameters.CheckForInvalidNgons = true;
+                parameters.CheckForNakedEdges = true;
+                parameters.CheckForNonManifoldEdges = true;
+                parameters.CheckForRandomFaceNormals = true;
+                parameters.CheckForSelfIntersection = true;
+                parameters.CheckForUnusedVertices = true;
+                Rhino.FileIO.TextLog log = new Rhino.FileIO.TextLog(Path.GetTempPath() + "mesh-checks.txt");
+                bool isValid = meshOut.Check(log, ref parameters);
+                bool hasInvalidVertexIndices = Helper.HasInvalidVertexIndices(meshOut);
+                if (!isValid || hasInvalidVertexIndices)
+                {
+                    RhinoApp.WriteLine("Total cound of disjoint meshes: {0}", parameters.DisjointMeshCount);
+                    RhinoApp.WriteLine("Output mesh cannot be added to scene due to being invalid");
+                }
+                else
+                {
+                    // If the mesh is valid, add it to the document.
+
+                    // Create a new object attributes with the desired name
+                    ObjectAttributes attributes = new ObjectAttributes();
+                    attributes.Name = "Hollowed: " + inObj.Attributes.Name;
+
+                    // Add the mesh to the document with the specified attributes
+                    docCurrent.Objects.AddMesh(meshOut, attributes);
+
+                    // Redraw the viewports to update the display
+                    docCurrent.Views.Redraw();
+
+                    if (docCurrent.Objects.Delete(inObj.Id, true))
+                    {
+                        // Good.
+                    }
+                    else
+                    {
+                        RhinoApp.WriteLine("Post process couldn't delete the original object.");
+                    }
+                }
+
+                RhinoApp.WriteLine("Post process finished.");
+            }
+
+            catch (Exception ex)
+            {
+                RhinoApp.WriteLine("Error on post process: {0}", ex.Message);
+            }
         }
     }
 }
---
+++ RhinoCommon/Hollow/Hollow/Paths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Hollow
+{
+    internal class Paths
+    {
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        // Input object to be saved as STL.
+        public static string stlIn = Path.GetTempPath() + "input.stl";
+        public static string stlOut = Path.GetTempPath() + "output.stl";
+
+        public static string logic = Path.Combine(AssemblyDirectory, "logic.exe");
+    }
+}
